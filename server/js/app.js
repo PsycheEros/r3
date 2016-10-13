b235596be8fbcb685c285608c93ed6d409a15bf0
error <error@error-unlimited.net>
2016-10-13 13:44:27 -0500

    serve client files

--- server/js/app.js
+++ server/js/app.js
@@ -1,7 +1,7 @@
 const express = require( 'express' ),
 	{ NODE_PORT = 3000, NODE_IP = 'localhost' } = process.env,
 	app = express(),
-	api = express.Router(); // eslint-disable-line new-cap
+	ws = express.Router(); // eslint-disable-line new-cap
 require( 'express-ws' )( app );
 
 app.get( '/health', ( req, res ) => {
@@ -9,13 +9,15 @@ app.get( '/health', ( req, res ) => {
 	res.end();
 } );
 
-api.ws( '/echo', ( ws, res ) => {
+ws.ws( '/echo', ( ws, res ) => {
 	ws.on( 'message', msg => {
 		ws.send( msg );
 	} );
 } );
 
-app.use( '/api', api );
+app.use( express.static( 'client' ) );
+
+app.use( '/ws', ws );
 
 app.listen( NODE_PORT, NODE_IP, () => {
 	console.log( `Application worker ${process.pid} started...` );
