2141eb9bb680d17d8397c6387bd254e14ba6e393
error <error@error-unlimited.net>
2016-10-23 00:10:43 -0500

    add fsm

---
+++ ts/fsm.ts
@@ -0,0 +1,46 @@
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { ArrayObservable } from 'rxjs/observable/ArrayObservable';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/defaultIfEmpty';
+import 'rxjs/add/operator/last';
+
+export class Fsm {
+	public addState( name: string ) {
+		const { stateMap } = this;
+		const state = new FsmState( name );
+		stateMap.set( name, state );
+		return state;
+	}
+
+	public setState( state: FsmState ) {
+		const { stateStack, stateSubject } = this;
+		stateStack[ Math.max( 0, stateStack.length - 1 ) ] = state;
+		stateSubject.next( state );
+	}
+
+	public pushState( state: FsmState ) {
+		const { stateStack, stateSubject } = this;
+		stateStack.push( state );
+		stateSubject.next( state );
+	}
+
+	public popState() {
+		const { stateStack, stateSubject } = this,
+			prevState = stateStack.pop(),
+			nextState = stateStack[ stateStack.length ] || null;
+		stateSubject.next( nextState );
+	}
+
+	public get currentState(): Observable<FsmState|null> { return this.stateSubject; }
+
+	private stateStack = [] as FsmState[];
+
+	private stateMap = new Map<string, FsmState>();
+
+	private stateSubject = new BehaviorSubject<FsmState|null>( null );
+}
+
+export class FsmState {
+	public constructor( public readonly name: string ) {} 
+}
