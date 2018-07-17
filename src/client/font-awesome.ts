4cbd3b3cbd9924d577f23c65fd16d69bdc941906
error <error@error-unlimited.net>
2018-07-17 11:39:41 -0500

    only import font-awesome icons we use

--- src/client/font-awesome.ts
+++ src/client/font-awesome.ts
@@ -1,5 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-library.add( fab, far, fas );
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+library.add( faTwitter );
+import { faComment } from '@fortawesome/free-regular-svg-icons';
+library.add( faComment );
+import { faDoorClosed, faDoorOpen, faKey, faLock, faTimes } from '@fortawesome/free-solid-svg-icons';
+library.add( faDoorClosed, faDoorOpen, faKey, faLock, faTimes );
