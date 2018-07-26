8dc71739dbd745b9c82fb6bdeb22cd52fe0a2260
error <error@error-unlimited.net>
2018-07-26 00:46:16 -0500

    fix import shadowed by parameter

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,6 +1,6 @@
 import { url, options } from 'data/redis.config.yaml';
 import Redis from 'ioredis';
 
-export function redis( options: Partial<Redis.RedisOptions> ) {
-	return new Redis( process.env.REDIS_URL || url, { ...options, ...options } );
+export function redis( opts: Partial<Redis.RedisOptions> ) {
+	return new Redis( process.env.REDIS_URL || url, { ...options, ...opts } );
 }
