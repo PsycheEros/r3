aaa96f1c96e3b211b1369465a31f3830c356cae2
error <error@error-unlimited.net>
2018-12-10 10:35:00 -0600

    observeOn

--- src/client/responsive.service.ts
+++ src/client/responsive.service.ts
@@ -1,15 +1,19 @@
-import { Injectable,  OnDestroy } from '@angular/core';
+import { Injectable,  OnDestroy, Inject } from '@angular/core';
 
-import { Subject, Observable, fromEvent } from 'rxjs';
-import { distinctUntilChanged, map, shareReplay, takeUntil } from 'rxjs/operators';
+import { Subject, Observable, fromEvent, SchedulerLike } from 'rxjs';
+import { distinctUntilChanged, map, shareReplay, takeUntil, observeOn } from 'rxjs/operators';
 import { breakpoints } from 'data/responsive.yaml';
+import { ZoneScheduler } from 'ngx-zone-scheduler/dist';
 
 @Injectable()
 export class ResponsiveService implements OnDestroy {
 	private readonly width: Observable<number>;
 	private readonly destroyed = new Subject<true>();
 
-	public constructor() {
+	public constructor(
+		@Inject(ZoneScheduler)
+		private readonly scheduler: SchedulerLike
+	) {
 		this.width =
 			fromEvent( window, 'resize', { passive: true } )
 			.pipe(
@@ -25,8 +29,9 @@ export class ResponsiveService implements OnDestroy {
 				for( const [ key, max ] of Object.entries( breakpoints ) ) {
 					if( width < max ) return key as ResponsiveBreakpoint;
 				}
-			} ),
-			distinctUntilChanged()
+			},
+			distinctUntilChanged(),
+			observeOn( this.scheduler )
 		);
 	}
 
